b4c863018c2d91a8002caadf75c33dea8c49ff9f
StaRa1n <1945159551@qq.com>
2023-12-01 21:33:15 +0800

    feat: init

---
+++ .vitepress/config.mjs
@@ -0,0 +1,36 @@
+import { text } from 'stream/consumers'
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'Yang的博客',
+  description: 'A VitePress Site',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+      {
+        text: '测试',
+        items: [
+          { text: '测试2', link: '/text' },
+          { text: '测试3', link: '/text' },
+        ],
+      },
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+    ],
+  },
+})
